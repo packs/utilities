c39fa901c4082828d4e6728ddf00cf5d754a8156
Scott Pack <scott.pack@gmail.com>
2014-05-17 15:14:19 -0400

    Fixed sorting and added option to reverse the output sorting

--- fast_sorter.pl
+++ fast_sorter.pl
@@ -1,17 +1,18 @@
 #!/usr/bin/perl -w
 
 use strict;
-use Getopt::Long;
+use Getopt::Long qw(:config bundling);
 use Pod::Usage;
 
 # Setup main variables from script arguments
-my ($unique, $sort, $count, $quiet, $DEBUG);
+my ($unique, $sort, $count, $reverse, $quiet, $DEBUG);
 
 # Grab the options passed on the command line.
 GetOptions (
   "unique|u" => \$unique,              # flag
   "sort|s" => \$sort,                  # flag
   "count|c" => \$count,                # flag
+  "reverse|r" => \$reverse,            # flag
   "quiet|q" => \$quiet,                # flag
   "verbose|v" => \$DEBUG,              # flag
   "help|?|h" => sub { pod2usage(1); }, # flag
@@ -21,29 +22,39 @@ if( !( defined($unique) or defined($sort) or defined($count) ) ) {
   pod2usage("$0:  Required argument missing.");
 }
 
+if( defined($reverse) and !( defined($sort) ) ) {
+    pod2usage("$0:  Cannot reverse sorting without sorting.");
+}
+
 my ( %hash, $key, $value );
 
 if ( $unique ) {
   print "Entered unique loop\n" if $DEBUG;
 
-  while( my $mac = <> ) {
-    next unless $mac;
-    chomp $mac;
+  while( my $line = <> ) {
+    next unless $line;
+    chomp $line;
 
-    $hash{$mac}++;
+    $hash{$line}++;
   }
 
   # Do all the prints here
   if ( $sort ) {
     print "Entered sorted print loop\n" if $DEBUG;
 
-    for $key ( sort keys %hash ) {
-      if ( $count ) {
+    if( $reverse ) {
+      foreach my $key (sort { $hash{$b} <=> $hash{$a} } keys %hash) {
         print "$hash{$key} \t$key\n";
       }
-      else {
-        print "$key\n";
+    }    
+    elsif( $count ) {
+      foreach my $key (sort { $hash{$a} <=> $hash{$b} } keys %hash) {
+        print "$hash{$key} \t$key\n";
       }
+    }
+    else
+    {
+      print "$key\n";
     }
   }
   else {
@@ -77,8 +88,9 @@ fast_sorter.pl [options]
 
  Options:
    -u, --unique     Deduplicate entries in the input
-   -s, --sort       Print the output ASCII sorted
+   -s, --sort       Print the output ASCII sorted based on counts
    -c, --count      Print the output along with duplication counts
+   -r, --reverse    Sorts output descending
    -v, --verbose    Print debugging information
    -h, --help       Brief help message
 
